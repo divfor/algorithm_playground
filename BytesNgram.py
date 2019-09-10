dabe966f2a33cb6fbe5c15f5b70f09ad59fc8942
Fred Huang <fred.huang@nexusguard.com>
2019-09-10 18:52:40 +0800

    update for else

--- BytesNgram.py
+++ BytesNgram.py
@@ -18,7 +18,7 @@ class BytesNgram(object):
         normal_capacity : default 10**8, ~137 MB
         seen_capacity : 2*10**8, ~274 MB
     '''
-    def __init__(self, n = 5, c = 'u2', max_wins = 100,
+    def __init__(self, n = 5, c = 'u2', max_wins = 100, bloom_dir='./'
                 gold_capacity = 10**8,
                 bad_capacity = 10**8,
                 normal_capacity = 10**8,
@@ -26,6 +26,7 @@ class BytesNgram(object):
         self.n = n # size for n-gram, default 5 bytes
         self.c = c # size for couter, default 2 bytes
         self.max_wins = max_wins
+        self.bloom_dir = bloom_dir
         self.gold_capacity = gold_capacity
         self.bad_capacity = bad_capacity
         self.normal_capacity = normal_capacity
@@ -42,10 +43,10 @@ class BytesNgram(object):
         dt = np.dtype([('counter', self.c), ('n-gram', bytes, self.n)])
         self.h = HashTop(dumpfile=gold_hashdb, lowfreq_threshold=1, highfreq_threshold=65535,
                         hash_size=self.gold_capacity, hash_dtype=dt)
-        self.gold = BloomFilterList(self.gold_capacity, 1/256, 'gold/gold_*.bloom')
-        self.seen = BloomFilterList(self.seen_capacity, 1/256, 'tmp/seen_*.bloom')
+        self.gold = BloomFilterList(self.gold_capacity, 1/256, self.bloom_dir + '/gold_*.bloom')
         self.gold.load()
-        self.seen.load()
+        #self.seen = BloomFilterList(self.seen_capacity, 1/256, self.bloom_dir + '/seen_*.bloom')
+        #self.seen.load()
         for payload in tqdm(payloads):
             try:
                 bts = bytes.fromhex(payload.strip()) # ba.unhexlify(hex_string)
@@ -56,12 +57,12 @@ class BytesNgram(object):
             num_wins = len(bts) - self.n + 1 # packet may be very large
             for i in range(min(num_wins, self.max_wins)):
                 self.ngrams += 1
-                ng = bts[i:i+n] # bytes type
-                self.h.add(ng) # hash counters
+                ng = bts[i:i+n]   # bytes type
+                self.h.add(ng)    # hash counters
                 self.gold.add(ng) # bloom filters
-                if not any([ng in b for b in self.seen.bflist]):
-                    self.seen.add(ng) # bloom filters for one-seen recording
-        self.seen.close()
+                #if not any([ng in b for b in self.seen.bflist]):
+                #    self.seen.add(ng) # bloom filters for one-seen recording
+        #self.seen.close()
         self.gold.close()
         self.h.close()
    
@@ -70,7 +71,7 @@ class BytesNgram(object):
         dt = np.dtype([('counter', self.c), ('n-gram', bytes, self.n)])
         self.h = HashTop(dumpfile=bad_hashdb, lowfreq_threshold=1, highfreq_threshold=65535,
                         hash_size=self.bad_capacity, hash_dtype=dt)
-        self.bad = BloomFilterList(self.bad_capacity, 1/256, 'bad/bad_*.bloom')
+        self.bad = BloomFilterList(self.bad_capacity, 1/256, self.bloom_dir + '/bad_*.bloom')
         for payload in tqdm(payloads):
             try:
                 bts = bytes.fromhex(payload.strip()) # ba.unhexlify(hex_string)
@@ -81,8 +82,8 @@ class BytesNgram(object):
             num_wins = len(bts) - self.n + 1 # packet may be very large
             for i in range(min(num_wins, self.max_wins)):
                 self.ngrams += 1
-                ng = bts[i:i+n] # bytes type
-                self.h.add(ng) # hash counters
+                ng = bts[i:i+n]  # bytes type
+                self.h.add(ng)   # hash counters
                 self.bad.add(ng) # bloom filters
         self.bad.close()
         self.h.close()
@@ -92,10 +93,10 @@ class BytesNgram(object):
         dt = np.dtype([('counter', self.c), ('n-gram', bytes, self.n)])
         self.h = HashTop(dumpfile=bad_hashdb, lowfreq_threshold=2, highfreq_threshold=65535,
                         hash_size=self.normal_capacity, hash_dtype=dt)
-        self.normal = BloomFilterList(self.normal_capacity, 1/256, 'normal/normal_*.bloom')
-        self.normal.load() # continue work
-        self.seen = BloomFilterList(self.seen_capacity, 1/256, 'tmp/seen_*.bloom')
-        self.seen.load()
+        self.normal = BloomFilterList(self.normal_capacity, 1/256, self.bloom_dir + '/normal_*.bloom')
+        self.normal.load()
+        #self.seen = BloomFilterList(self.seen_capacity, 1/256, self.bloom_dir + '/seen_*.bloom')
+        #self.seen.load()
         for payload in tqdm(payloads):
             try:
                 bts = bytes.fromhex(payload.strip()) # ba.unhexlify(hex_string)
@@ -106,24 +107,22 @@ class BytesNgram(object):
             num_wins = len(bts) - self.n + 1 # packet may be very large
 
             # if 5+% same as bad filter, give up this payload
-            p5threshold = int(0.05 * num_wins)
-            in_bad_filters = 0
+            soft_deny = int(0.05 * num_wins)
+            count = 0
             for i in range(num_wins):
                 if any([bts[i:i+self.n] in bf for bf in self.bad.bflist]):
-                    in_bad_filters += 1
-                if in_bad_filters > p5threshold:
+                    count += 1
+                if count > soft_deny:
                     break
-            if in_bad_filters > p5threshold:
-                continue
-
-            for i in range(min(num_wins, self.max_wins)):
-                self.ngrams += 1
-                ng = bts[i:i+n] # bytes type
-                self.h.add(ng) # hash counters
-                self.normal.add(ng) # bloom filters
-                if not any([ng in b for b in self.seen.bflist]):
-                    self.seen.add(ng) # bloom filters for one-seen recording
+            else:
+                for i in range(min(num_wins, self.max_wins)):
+                    self.ngrams += 1
+                    ng = bts[i:i+n]     # bytes type
+                    self.h.add(ng)      # hash counters
+                    self.normal.add(ng) # bloom filters
+                    #if not any([ng in b for b in self.seen.bflist]):
+                    #    self.seen.add(ng) # bloom filters for one-seen recording
+        #self.seen.close()
         self.normal.close()
-        self.seen.close()
         self.h.close()
 
